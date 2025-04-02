6784e4fa6f99d58b798489cfe6b51ce89dc471ef
Viraj130 <154680252+Viraj130@users.noreply.github.com>
2025-04-02 08:31:27 +0530

    2-24-25

---
+++ app1/src/components/Junagadh.js
@@ -0,0 +1,16 @@
+import {Component} from "react";
+
+class Junagadh extends Component{
+  constructor(){
+    super();
+  }
+
+  render(){
+    return<>
+    <h1>Welcome to Junagadh</h1>
+    <p>With Class Component</p>
+    </>
+  }
+}
+
+export default Junagadh;
